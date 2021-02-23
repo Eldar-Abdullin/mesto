b518c799fe5766f95644bc9b98f5b479e47d2499
Eldar Abdullin <shpast@yandex.ru>
2021-02-23 12:37:22 +0500

    Проектная работа 4 код ревью 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,25 @@
+//Присваиваем значения
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
-let popupButton = document.querySelector('.popup__button');
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
 let popupOpenElement = document.querySelector('.popup')
 let profileName = document.querySelector('.profile__name')
 let profileJob = document.querySelector('.profile__job')
+// делаем функцию открытия popup 
+// и сразу вставляем данные в поля ввода из профиля
 function openPopup() {
     popupOpenElement.classList.add('popup_opened');
-    nameInput.value = profileName.innerHTML;
-    jobInput.value = profileJob.innerHTML;
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
-editButton.addEventListener('click', openPopup);
+// делаем фунцкию закрытия popup
 function closePopup() {
     popupOpenElement.classList.remove('popup_opened');
 }
-closeButton.addEventListener('click', closePopup);
+// делаем фунцкию отправки данных из popup 
+// в профиль
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
@@ -24,3 +27,6 @@ function formSubmitHandler (evt) {
     closePopup();
 }
 formElement.addEventListener('submit', formSubmitHandler); 
+// отслеживаемые события
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
